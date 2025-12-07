2d85dfe6532b70364a823e1749b1e834464006f4
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-07 03:44:20 +0100

    cleaned merge markers

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
 import tailwindcss from '@tailwindcss/vite'
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss() ],
-=======
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> b397e114b29c692f3e5d4107494cbbe875ec7c6a
 })
