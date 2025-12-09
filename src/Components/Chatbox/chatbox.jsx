732859ace5eea5351528f7ef95d4ae040bc73d13
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-09 23:22:58 +0100

    Corrected error in chatbot code

---
+++ src/Components/Chatbox/chatbox.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import './chatbox.css' ;
+
+
+const ChatMess = ({message, sender}) => {
+    return(
+        <>
+        <section className='py-8 px-4 m-4 '> 
+       <div className='bg-green-200'>
+        {sender === "bot" && <img src="../src/assets/chatbot.png" alt="The chatbot image" /> }
+        {message}
+        {sender === "user" && <img src="../src/assets/user.png" alt="The user's image" /> }
+        </div>
+        </section>
+        </>
+    )}
+
+       const Chatbox = () => {
+     const [chatMessages, setChatMessages] = React.useState([{
+           id : "1",
+           message: "Hello chat",
+           sender: "user"}, 
+        {    id : "2",
+            message: "hello ! may i help you today date ?" ,
+            sender : "bot"
+    }, 
+    {          id : "3",
+             message : "can you help you me with today's date." ,
+            sender : "user"
+    }, 
+    {        id : "4",
+          message : "Today's date is october 1" ,
+          sender : "bot"
+    },
+     {
+          id :  "5",
+        message : "oh okay  thank you !" ,
+        sender :  "user"
+    },
+    {    id     :      "6",
+        message : "You are welcome, how can i help you further or that will be all for now !" ,
+        sender : "bot"
+    }
+] )
+
+          function  messageChat() {
+            setChatMessages( [...chatMessages,{
+                message : "okay, i think i have more questions." ,
+                sender : "user",
+                id : crypto.randomUUID()
+            }, {
+                      message: "Okay please, go ahead !!",
+                      sender: "bot",
+                      id: crypto.randomUUID()
+
+            }] )
+          }
+
+        return (    
+              <>                  
+           <input type="text" placeholder='send me a message now' size={30} />
+            <button onClick={messageChat}> click me </button>
+           {chatMessages.map((chatMessage) => {
+            return ( 
+              <ChatMess key={chatMessage.id} sender={chatMessage.sender} message={chatMessage.message}  />
+            )
+          })}
+    </>
+)
+}
+
+
+
+
+export default Chatbox
