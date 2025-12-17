bbdb7da6f6072c599e397617a859b21e70388947
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-17 23:52:34 +0100

    corrected error in chatbox.jsx file

--- src/Components/Chatbox/chatbox.jsx
+++ src/Components/Chatbox/chatbox.jsx
@@ -29,7 +29,7 @@ const ChatMess = ({message, sender}) => {
     }, 
     {        id : "4",
           message : "Today's date is october 1" ,
-          sender : "bot"\\\\\\\\rehty4 tnfdncp23rmwexsa
+          sender : "bot"
     },
     {
           id :  "5",
