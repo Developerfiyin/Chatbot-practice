96d1943639cde20b8c421ef6ef58aaf2dc542bb8
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-18 23:13:26 +0100

    corrected error

--- src/Components/Chatbox/chatbox.jsx
+++ src/Components/Chatbox/chatbox.jsx
@@ -72,7 +72,7 @@ const ChatMess = ({message, sender}) => {
              onClick={messageChat}>Click me</button>
            {chatMessages.map((chatMessage) => {
             return ( 
-              <ChatMes
+              <ChatMess key={chatMessage.id} sender={chatMessage.sender} message={chatMessage.message}/>
             )
           })}
 
